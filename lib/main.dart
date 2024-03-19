15b14418120ebcfc00fe8a2b1485fd3680f34b0f
olanrewajutaiwo185 <olanrewajutaiwo185@gmail.com>
2024-03-19 01:04:59 +0000

    updates on home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:myapp/splash_screen.dart';
 
 void main() {
@@ -11,15 +12,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Golemon',
-      theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const SplashScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (_ , child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Gomeal',
+          // You can use the library anywhere in the app even in theme
+          // theme: ThemeData(
+          //   primarySwatch: Colors.blue[100],
+          //   textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          // ),
+          home: child,
+        );
+      },
+      child: const SplashScreen(),
     );
   }
 }
--- lib/src/features/Marketplace/pages/Home/Screens/Home_screen.dart
+++ lib/src/features/Marketplace/pages/Home/Screens/Home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,13 +15,25 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: Column(
         children: [
-          
+          SizedBox(height: 40.h,),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: ListTile(
               leading: CircleAvatar(),
-              
-              
+              title: Text(
+                    'Welcome back 👋 ',
+                    style: GoogleFonts.poppins(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                    ),
+                    ),
+              subtitle:  Text(
+                    'Olajide williams',
+                    style: GoogleFonts.poppins(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500
+                    ),
+                    ),
             ),
           ),
         ],
