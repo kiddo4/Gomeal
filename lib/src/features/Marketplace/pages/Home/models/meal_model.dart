78fa6e647c9fbc1496e9d1e47a5f10cb84dc75e2
olanrewajutaiwo185 <olanrewajutaiwo185@gmail.com>
2024-03-19 04:18:03 +0000

    updates

--- lib/src/features/Marketplace/pages/Home/models/meal_model.dart
+++ lib/src/features/Marketplace/pages/Home/models/meal_model.dart
@@ -20,28 +20,28 @@ final meals = [
     image: 'https://www.chicken-republic.com/wp-content/uploads/2021/10/CITIZENS-SPICY-YAM-MEAL-DARK.jpg',
     mealName: 'chicken and chips',
     mealSeller: 'Chicken republic',
-    sellerImage: 'https://example.com/seller1.jpg', // Seller 1's image URL
+    sellerImage: 'https://pbs.twimg.com/profile_images/1314397282628972544/6errNiqW_400x400.jpg', // Seller 1's image URL
   ),
   MealModel(
     price: '150',
     image: 'https://www.chicken-republic.com/wp-content/uploads/2021/09/ChickenRepublic_QuarterRotisserie.jpg',
     mealName: 'Chicken',
     mealSeller: 'Chicken Republic',
-    sellerImage: 'https://example.com/seller2.jpg', // Seller 2's image URL
+    sellerImage: 'https://pbs.twimg.com/profile_images/1314397282628972544/6errNiqW_400x400.jpg', // Seller 2's image URL
   ),
   MealModel(
     price: '25000',
     image: 'https://www.chicken-republic.com/wp-content/uploads/2021/09/ChickenRepublic_RefuelDodoMaxMeal.jpg',
     mealName: 'Rice and dodo',
     mealSeller: 'Chicken repblic',
-    sellerImage: 'https://example.com/seller2.jpg', // Seller 2's image URL
+    sellerImage: 'https://pbs.twimg.com/profile_images/1314397282628972544/6errNiqW_400x400.jpg', // Seller 2's image URL
   ),
   MealModel(
     price: '150',
     image: 'https://www.chicken-republic.com/wp-content/uploads/2021/10/CITIZENS-SPICY-YAM-MEAL-DARK.jpg',
     mealName: 'Meal 2',
     mealSeller: 'Chicken republic',
-    sellerImage: 'https://example.com/seller2.jpg', // Seller 2's image URL
+    sellerImage: 'https://pbs.twimg.com/profile_images/1314397282628972544/6errNiqW_400x400.jpg', // Seller 2's image URL
   ),
   // Add more MealModel instances as needed...
 ];
